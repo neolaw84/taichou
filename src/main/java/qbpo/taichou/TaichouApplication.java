b5ce0d919c4060d808d2a034ba9070eff8a66f91
neolaw84 <neolaw@gmail.com>
2016-10-08 13:06:53 +0800

    Initial commit

---
+++ src/main/java/qbpo/taichou/TaichouApplication.java
@@ -0,0 +1,12 @@
+package qbpo.taichou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaichouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaichouApplication.class, args);
+	}
+}
